d610655a3970b5c7bbb41d947e32be327764f58d
kaan3392 <kaanalacali@gmail.com>
2022-06-04 19:12:11 +0300

    comment return eklendi

--- routes/comment.js
+++ routes/comment.js
@@ -13,37 +13,10 @@ router.post("/", verifyToken, async (req, res) => {
       { _id: req.body.productId },
       { $push: { comments: saved._id } }
     );
-    res.status(200).json("thanks for your opinion");
+   return res.status(200).json("thanks for your opinion");
   } catch (err) {
-    res.status(500).json(err);
+   return res.status(500).json(err);
   }
 });
-
-//get comments of a product
-
-// router.get("/:productId", async (req, res) => {
-//   const { rating } = req.query;
-//   try {
-//     let comments;
-//     if (rating) {
-//       comments = await Comment.aggregate([
-//         { $match: { productId: req.params.productId } },
-//         {
-//           $group: {
-//             _id: null,
-//             avg: { $avg: "$review" },
-//           },
-//         },
-//       ]);
-//     } else {
-//       comments = await Comment.find({ productId: req.params.productId }).sort({
-//         createdAt: -1,
-//       });
-//     }
-//     res.status(200).json(comments);
-//   } catch (err) {
-//     res.status(500).json("Oops! Somethings went so so wrong...");
-//   }
-// });
 
 module.exports = router;
